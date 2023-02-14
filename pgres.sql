da31d54c3dc5979f667ec45e3f615f2a8e769663
Jake <jakedavisemail@gmail.com>
2023-02-14 14:36:19 -0700

    qna PostgreSQL schema

---
+++ pgres.sql
@@ -0,0 +1,22 @@
+CREATE TABLE questions (
+  question_id SERIAL NOT NULL PRIMARY KEY,
+  question_body VARCHAR(300),
+  question_date DATE,
+  asker_name VARCHAR(50),
+  question_helpfulness INT,
+  reported BOOLEAN
+);
+CREATE TABLE answers (
+  asnwer_id SERIAL NOT NULL PRIMARY KEY,
+  question_id INT NOT NULL,
+  body VARCHAR(300),
+  date DATE,
+  answerer_name VARCHAR(50),
+  helpfulness INT,
+  reported BOOLEAN
+);
+CREATE TABLE photos (
+  photo_id SERIAL NOT NULL PRIMARY KEY,
+  answer_id INT NOT NULL,
+  photo_url VARCHAR(100)
+);
