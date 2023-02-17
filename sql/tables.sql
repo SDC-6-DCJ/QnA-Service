23705cc2b422fa16035b938888ad65674b7b7c10
Jake <jakedavisemail@gmail.com>
2023-02-16 17:38:03 -0700

    Better ETL

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,26 +4,36 @@
   For Jake - psql qna -f Galvanize/seniorProjects/QnA-Service/pgres.sql
 */
 
-CREATE TABLE questions (
-  question_id SERIAL NOT NULL PRIMARY KEY,
-  question_body VARCHAR(300),
-  question_date DATE,
+CREATE TABLE IF NOT EXISTS questions (
+  id SERIAL NOT NULL PRIMARY KEY,
+  product_id INT NOT NULL,
+  body VARCHAR(300),
+  date BIGINT,
   asker_name VARCHAR(50),
-  question_helpfulness INT,
-  reported BOOLEAN
+  asker_email VARCHAR(100),
+  reported BOOLEAN,
+  helpful SMALLINT
 );
-CREATE TABLE answers (
+CREATE INDEX IF NOT EXISTS product_id_index ON questions(product_id);
+
+
+CREATE TABLE IF NOT EXISTS answers (
   id SERIAL NOT NULL PRIMARY KEY,
   question_id INT NOT NULL,
   body VARCHAR(300),
-  date_written DATE,
+  date BIGINT,
   answerer_name VARCHAR(50),
-  answerer_email VARCHAR(50),
+  answerer_email VARCHAR(100),
   reported BOOLEAN,
-  helpfulness INT
+  helpful SMALLINT
 );
-CREATE TABLE photos (
+CREATE INDEX IF NOT EXISTS question_id_index ON answers(question_id);
+
+
+CREATE TABLE IF NOT EXISTS photos (
   id SERIAL NOT NULL PRIMARY KEY,
   answer_id INT NOT NULL,
-  url VARCHAR(100)
+  url VARCHAR(1000) NOT NULL
 );
+CREATE INDEX IF NOT EXISTS answer_id_index ON photos(answer_id);
+
